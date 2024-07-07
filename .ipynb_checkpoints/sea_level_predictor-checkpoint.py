b7ec77457fed4dde582585f964004bb90bc0917c
= <=>
2024-07-07 13:27:12 +0530

    Completed Sea_level_precdictor

--- .ipynb_checkpoints/sea_level_predictor-checkpoint.py
+++ .ipynb_checkpoints/sea_level_predictor-checkpoint.py
@@ -13,9 +13,9 @@ def draw_plot():
 
 
     # Create first line of best fit
-    res1 = linregress(df['Year'],df['CSIRO Adjusted Sea Level'])
+    res1 = stats.linregress(df['Year'],df['CSIRO Adjusted Sea Level'])
     y1=res1.intercept + res1.slope*df['Year']
-    x1=pd.Series(range(1880,2051))
+    x1=np.arange(1880,2051,1)
     y1=list(res1.intercept + res1.slope*x1)
     ax.plot(x1,y1,'r',label='First Line')
     
@@ -23,8 +23,8 @@ def draw_plot():
 
     # Create second line of best fit
     df_filtered=df[df['Year']>=2000]
-    res2=linregress(df_filtered['Year'],df_filtered['CSIRO Adjusted Sea Level'])
-    x2=pd.Series(range(2000,2051))
+    res2=stats.linregress(df_filtered['Year'],df_filtered['CSIRO Adjusted Sea Level'])
+    x2=np.arange(2000,2051,1)
     y2=list(res2.intercept + res2.slope*x2)
     ax.plot(x2,y2,'g',label='Second Line')
 
